852738b599408b853dfce4b19ebd4fc235b7b0bd
st58583 <delta.fixer@seznam.cz>
2024-05-13 21:33:52 +0200

    Add files via upload

--- pages/vydej/porizeni/inc/arg.php
+++ pages/vydej/porizeni/inc/arg.php
@@ -1,1 +1,10 @@
-
+<?php
+if (g_arg(0) == "storno" && g_arg(1)) {
+	$res = sql("SELECT bii_item FROM bill_items WHERE bii_bill = '". g_arg(1) ."'");
+	while ($row = sql_obj($res)) sql("UPDATE storage_items SET sti_available = 'Y' WHERE sti_item = '". $row->bii_item ."' AND sti_available = 'N'");
+	sql("UPDATE bill_items SET bii_bill = '0' WHERE bii_bill = '". g_arg(1) ."'");
+	sql("DELETE FROM bills WHERE bil_id = '". g_arg(1) ."'");
+	
+	Location('../');
+}
+?>
