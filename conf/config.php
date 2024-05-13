32608f7f376e4de93b8b364c3397eceb7946777c
st58583 <delta.fixer@seznam.cz>
2024-05-13 21:19:59 +0200

    Add files via upload

---
+++ conf/config.php
@@ -0,0 +1,9 @@
+<?php
+#DB
+const MYSQL_server = '--DB_server--';
+const MYSQL_login = '--DB_login--';
+const MYSQL_password = '--DB_password--';
+const MYSQL_db = '--DB_nazev--';
+#SERVER
+const SERVER = 'localhost/sklad'; //edit to suit your domain
+?>
